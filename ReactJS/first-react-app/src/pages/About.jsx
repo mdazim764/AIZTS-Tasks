0b4f57db6124681058acce512becbbb3361be05a
Azim Munawwar Gani Khairdi <58516056+mdazim764@users.noreply.github.com>
2025-08-21 14:08:07 +0530

    Update About.jsx

--- ReactJS/first-react-app/src/pages/About.jsx
+++ ReactJS/first-react-app/src/pages/About.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const About = () => {
+  const navigate = useNavigate();
+
+  const handleNavigation = () => {
+    navigate('/product');
+  }
+  const gotoHome = () => {
+    navigate('/');
+  } 
+  const handleBack = () => {
+    navigate(-1);
+  }
+
   return (
-    <div>About</div>
+    <div>
+      <h2>About</h2>
+      <div style={{ marginTop: '20px', display: 'flex', gap: '10px' }}>
+      <button onClick={handleNavigation}>Go to Product List</button>
+      <button onClick={gotoHome}>Go to Home</button>
+      <button onClick={handleBack}>Go Back</button>
+      </div>
+    </div>
   )
 }
 
